42b879cc314568171b8516e1a47810c8fd409b87
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2016-11-20 02:22:07 -0500

    Load the doom plus limits config variable

--- src/doom/g_game.c
+++ src/doom/g_game.c
@@ -229,6 +229,7 @@ int		bodyqueslot;
  
 int             vanilla_savegame_limit = 1;
 int             vanilla_demo_limit = 1;
+int             doom_plus_limits = 0;
  
 int G_CmdChecksum (ticcmd_t* cmd) 
 { 
